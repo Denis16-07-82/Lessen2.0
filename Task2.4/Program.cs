241c25c1a3319dff35be3786206b6cf4860dbc9f
DenisV <naum2891@mail.ru>
2022-09-04 23:21:49 +0300

    add "Проверка кратности случайного числа на перёд заданному "

---
+++ Task2.4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 5 *: Напишите программу, которая генерирует 
+// несколько случайных чисел, и в цикле проверяет,
+//  кратны ли эти числа предварительно введенному числу, при кратности - цикл прерывается.
+// Введенное число 2
+// 13 -> нет
+// 15 -> нет
+// 12 -> да
+int Prompt(string messeg)
+{
+    System.Console.WriteLine(messeg);
+    string strValue = System.Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+
+}
+int valueA = Prompt("Введите желаемое число : ");
+while(1 == 1)
+{
+    int value = new Random().Next();
+    if(value % valueA == 0)
+    {
+        System.Console.WriteLine($" Случайное число: {value} кратно числу ,введённому пользователем: {valueA}");
+        break;
+    }
+    else
+    {
+        System.Console.WriteLine($" Случайное число: {value} не кратно числу ,введённому пользователем: {valueA}");
+    }
+}
