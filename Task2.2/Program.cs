203c5057cdc7a5427a80417368e6b6e253c9bc65
DenisV <naum2891@mail.ru>
2022-09-04 22:02:09 +0300

    add "Удаление второй цифры из трёхзначного числа. "

---
+++ Task2.2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное 
+// трёхзначное число и удаляет вторую цифру этого числа
+
+int Prompt(string messeg)
+{
+    int value = new Random().Next(100, 999);
+    System.Console.WriteLine($"Случайное трёхзначное число : {value} ");
+    return value;
+
+}
+int valueA = Prompt("Случайное трёхзначное число");// Просто мне так захотелось сделать ,чтоб привычка вводить функции была.
+int valueB = (valueA % 10) + ((valueA - (valueA % 100)) / 10);
+System.Console.WriteLine($"Полученое число после удаления второй цифры : {valueB} ");
